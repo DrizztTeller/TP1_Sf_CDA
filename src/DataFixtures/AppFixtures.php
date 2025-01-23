fa28477e758bf5d7aa859a4a3a54e8e3e8b48267
Mohammad ABDUL <abdul.opto@gmail.com>
2025-01-23 11:39:22 +0100

    Rajout des users et des posts

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,9 +3,10 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Post;
+use App\Entity\User;
 use App\Entity\Event;
 use App\Entity\Speaker;
-use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -13,15 +14,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create('fr_FR'); // Initialisation de Faker
+        $faker = Factory::create('en_EN'); // Initialisation de Faker
 
         // Création d'un utilisateur admin
-        $user = new User();
-        $user->setEmail('admin@admin.fr')
+        $admin = new User();
+        $admin->setEmail('admin@admin.fr')
             ->setPassword('$2y$13$NJpGg/WaTYG0ONkZkf6tvuPVmkuexwRQqozQKsp5b8yc9z9B3ziMG') // admin
+            ->setNickname('Administrator_' . uniqid())
             ->setRoles(['ROLE_ADMIN'])
-            ;
-        $manager->persist($user);
+        ;
+        $this->addReference('user_admin', $admin);
+        $manager->persist($admin);
 
         // Création de 40 speakers
         $speakerImages = [
@@ -31,7 +34,7 @@ class AppFixtures extends Fixture
             'user4.jpg',
         ];
         $speakerArray = [];
-        for ($i=1; $i < 41; $i++) { 
+        for ($i = 1; $i < 41; $i++) {
             $speaker = new Speaker();
             $speaker->setFirstname($faker->firstName)
                 ->setLastname($faker->lastName)
@@ -39,9 +42,9 @@ class AppFixtures extends Fixture
                 ->setCompany($faker->company)
                 ->setExperience($faker->numberBetween(1, 20))
                 ->setImage($faker->randomElement($speakerImages))
-                ;
-                array_push($speakerArray, $speaker);
-                $manager->persist($speaker);
+            ;
+            array_push($speakerArray, $speaker);
+            $manager->persist($speaker);
         }
 
         // Tableau de 20 événements
@@ -69,7 +72,7 @@ class AppFixtures extends Fixture
         ];
 
         // Boucle pour créer 20 événements
-        for ($i=0; $i < count($events); $i++) { 
+        for ($i = 0; $i < count($events); $i++) {
             $event = new Event();
             $event->setName($events[$i])
                 ->setTheme('Web development')
@@ -78,10 +81,46 @@ class AppFixtures extends Fixture
                 ->setAttendee($faker->numberBetween(10, 100))
                 ->setPrice($faker->numberBetween(0, 250))
                 ->addSpeaker($speakerArray[$i])
-                ;
+            ;
             $manager->persist($event);
         }
-        
+
+
+        // Ajouts des users
+        for ($i = 0; $i < 300; $i++) {
+            $user = new User();
+            $user
+                ->setNickname($faker->userName . '_' . uniqid())
+                ->setEmail($faker->email)
+                ->setPassword('$2y$13$rIVlC64Za/GIa4fm5/YMXuK6vKWAjPQ6J0F1DoQql2EP04tUUkvrW')
+                ->setRoles(['ROLE_USER'])
+            ;
+
+            $this->addReference('user_' . $i, $user);
+            $manager->persist($user);
+        }
+
+
+        $userArray = [];
+        for ($i = 0; $i < 300; $i++) {
+            $userArray[] = $this->getReference('user_' . $i, User::class);
+        }
+
+        // Ajout des posts
+        for ($i = 0; $i < 500; $i++) {
+            $user = $faker->randomElement($userArray);
+            $post = new Post();
+            $post
+                ->setTitle($faker->sentence(3))
+                ->setContent($faker->paragraph(3))
+                ->setIsPublished($faker->boolean(70))
+                ->setImage('https://picsum.photos/1280/720?random=' . $i)
+                ->setRef(uniqid())
+                ->setAuthor($user)
+            ;
+            $manager->persist($post);
+        }
+
         $manager->flush();
     }
 }
